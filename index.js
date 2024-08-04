a84a7a87ba14c1852dc6c3c2456c4eaf7168f487
RTXG26 <69777967+RTXG26@users.noreply.github.com>
2024-08-05 01:33:56 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const imgContainer = document.querySelector(".image__container")
+const dotsContainer = document.querySelector(".nav__dots")
+
+const headerDiv = document.getElementById("headerDiv")
+const carouselDiv1 = document.getElementById("carouselContDiv1")
+const carouselDiv2 = document.getElementById("carouselContDiv2")
+const skillDiv = document.getElementById("skillDiv")
+const youtubeDiv = document.getElementById("youtube-div")
+const upcomingDiv = document.getElementById("upcoming-eventsDiv")
+const skillDiv2 = document.getElementById("skillDiv2")
+
+
+document.getElementById("dot1").addEventListener("click", () => {
+  imgContainer.className = "image__container first-active"
+  dotsContainer.className = "nav__dots first-current"
+})
+document.getElementById("dot2").addEventListener("click", () => {
+  imgContainer.className = "image__container second-active"
+  dotsContainer.className = "nav__dots second-current"
+})
+document.getElementById("dot3").addEventListener("click", () => {
+  imgContainer.className = "image__container third-active"
+  dotsContainer.className = "nav__dots third-current"
+})
+
+
+function callme() {
+  if (window.matchMedia("(min-width:768px)").matches) {
+    headerDiv.classList.add("container")
+    carouselDiv1.classList.add("container")
+    carouselDiv2.classList.add("container")
+    skillDiv.classList.add("container")
+    youtubeDiv.classList.add("container")
+    upcomingDiv.classList.add("container")
+    skillDiv2.classList.add("container")
+    //carouselDiv
+    //carouselDiv2
+  } else {
+
+
+    headerDiv.classList.remove("container")
+    carouselDiv1.classList.remove("container")
+    carouselDiv2.classList.remove("container")
+    skillDiv.classList.remove("container")
+    youtubeDiv.classList.remove("container")
+    upcomingDiv.classList.remove("container")
+    skillDiv2.classList.remove("container")
+    //carouselDiv2.classList.add("container-fluid")
+  }
+}
+
+window.onresize = function () {
+  callme()
+}
